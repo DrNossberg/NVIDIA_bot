3bfc0482099218392e2957793b2a4a393db29925
DrNossberg <titou210@hotmail.fr>
2021-09-27 18:36:37 +0200

    Removing TeaParty Servers from the software

--- Discord.py
+++ Discord.py
@@ -10,17 +10,16 @@ import json
 # 1 STD OUTPUT ONLY
 # 2 BOTH BOT + STD OUTPUT
 
-# Set up environment variables and constants. Do not modify this unless you know what you are doing!
-Serveur_Panda = 'https://discord.com/api/webhooks/797638558093017169/NkL-dl3Zb2v0hT8G_hc4-KrKH-ZWaNz713X_iCqqjbYR7EW8ANMNtLNFC7dz76LeQ-Fn'
-TeaParty = "https://discord.com/api/webhooks/798686087672758292/vq01IiQ1e2T87BfbMfWbyj5z6dup7iYfQGP7qZM841NVyo4N9-mI44jgkRxQGFwSJqmi"
+# Set up environment variables and constants
+# You have to set this one up, depending on your server ! 
+Your_discord_server_webhook = "https://discord.com/api/webhooks/798686087672758292/vq01IiQ1e2T87BfbMfWbyj5z6dup7iYfQGP7qZM841NVyo4N9-mI44jgkRxQGFwSJqmi"
 
 def discord_notification(product):
     data = {
         "content": "{}".format(product),
         "username": "TARS"
     }
-    result = requests.post(TeaParty, data=json.dumps(data), headers={"Content-Type": "application/json"})
-    result = requests.post(Serveur_Panda, data=json.dumps(data), headers={"Content-Type": "application/json"})
+    result = requests.post(Your_discord_server_webhook, data=json.dumps(data), headers={"Content-Type": "application/json"})
     try:
         result.raise_for_status()
     except requests.exceptions.HTTPError as err:
